ffe1723429ed948e6029a7f558b1a88fbd8fe673
chrystian <chrystianr@gmail.com>
2015-08-31 20:04:15 +0200

    updating gulp tasks

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -11,37 +11,37 @@ var webpackConfig = require('./webpack.config.js');
 var webpackLibsConfig = require('./webpack.libs.config.js');
 
 var paths = {
-  sass: ['./src/scss/styles.scss'],
-  sources_js: ['./src/js/app.js', './src/js/**/*.js'],
-  libs_js: ['./src/libs/index.js', './src/libs/**/*.js'],
-  sources_html: ['./src/js/**/templates/*.html']
+    sass: ['./src/scss/styles.scss'],
+    sources_js: ['./src/js/app.js', './src/js/**/*.js'],
+    libs_js: ['./src/libs/index.js', './src/libs/**/*.js'],
+    sources_html: ['./src/js/**/templates/*.html']
 };
 
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function (done) {
-  gulp.src(paths.sass)
-          .pipe($.sass ({
+    gulp.src(paths.sass)
+        .pipe($.sass ({
             errLogToConsole: true
-          }))
-          .pipe(gulp.dest('./www/css/'))
-    /*
-     .pipe(minifyCss({
-     keepSpecialComments: 0
-     }))
-     .pipe(rename({ extname: '.min.css' }))
-     .pipe(gulp.dest('./www/css/'))
-     */
-          .on('end', done);
+        }))
+        .pipe(gulp.dest('./www/css/'))
+        /*
+         .pipe(minifyCss({
+         keepSpecialComments: 0
+         }))
+         .pipe(rename({ extname: '.min.css' }))
+         .pipe(gulp.dest('./www/css/'))
+         */
+        .on('end', done);
 });
 
 gulp.task('jscs', function () {
-  gulp.src(paths.scripts_js)
-          .pipe($.jscs())
-          .pipe($.notify({
+    gulp.src(paths.scripts_js)
+        .pipe($.jscs())
+        .pipe($.notify({
             title: 'JSCS',
             message: 'JSCS Passed. Let it fly!'
-          }));
+        }));
 });
 
 gulp.task('lint', function () {
@@ -50,50 +50,54 @@ gulp.task('lint', function () {
         .pipe($.jshint.reporter('jshint-stylish'))
         .pipe($.jshint.reporter('fail'))
         .pipe($.notify({
-          title: 'JSHint',
-          message: 'JSHint Passed. Let it fly!'
+            title: 'JSHint',
+            message: 'JSHint Passed. Let it fly!'
         }));
 });
 
 // Scripts
 gulp.task('scripts', function () {
-  return gulp.src(paths.sources_js)
+    return gulp.src(paths.sources_js)
     // .pipe(jshint(''))
     // .pipe(jshint.reporter('default'))
-          .pipe($.sourcemaps.init())
-          .pipe($.babel())
-          .pipe($.concat('scripts.js'))
-          .pipe($.sourcemaps.write())
-          .pipe(gulp.dest('www/js'))
+    .pipe($.sourcemaps.init())
+    .pipe($.babel())
+    .on('error', function (error) {
+        console.log(error.toString());
+        this.emit('end');
+    })
+    .pipe($.concat('scripts.js'))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('www/js'))
 
-          .pipe($.notify({message: 'Scripts task complete'}));
+    .pipe($.notify({message: 'Scripts task complete'}));
 });
 
 gulp.task('templates', function () {
     gulp.src(paths.sources_html)
-      .pipe($.minifyHtml())
-      .pipe($.ngHtml2js({
-        moduleName: 'templatesModule',
-        rename: function (templateUrl, templateFile) {
-          return templateUrl.replace('/templates', '');
-        }
-      }))
-      .pipe($.concat('templates.js'))
-      .pipe(gulp.dest('./www/js'));
+        .pipe($.minifyHtml())
+        .pipe($.ngHtml2js({
+            moduleName: 'templatesModule',
+            rename: function (templateUrl) {
+                return templateUrl.replace('/templates', '');
+            }
+        }))
+        .pipe($.concat('templates.js'))
+        .pipe(gulp.dest('./www/js'));
 });
 
 gulp.task('webpack-libs', function (callback) {
-  webpack(webpackLibsConfig, function (err, stats) {
-    if (err) {
-      throw new gutil.PluginError('webpack', err);
-    }
+    webpack(webpackLibsConfig, function (err, stats) {
+        if (err) {
+            throw new gutil.PluginError('webpack', err);
+        }
 
-    gutil.log('[webpack]', stats.toString({
-      colors: true
-    }));
+        gutil.log('[webpack]', stats.toString({
+            colors: true
+        }));
 
-    callback();
-  });
+        callback();
+    });
 });
 
 gulp.task('wds', [
@@ -103,7 +107,7 @@ gulp.task('wds', [
     'sass',
     'watch'
 ],
-function (callback) {
+function () {
     new WebpackDevServer(webpack(webpackConfig), {
         contentBase: 'www',
         stats: {
@@ -122,7 +126,7 @@ function (callback) {
 });
 
 gulp.task('watch', function () {
-  gulp.watch(paths.sass, ['sass']);
-  gulp.watch(paths.sources_html, ['templates']);
-  gulp.watch(paths.sources_js, ['scripts']);
+    gulp.watch(paths.sass, ['sass']);
+    gulp.watch(paths.sources_html, ['templates']);
+    gulp.watch(paths.sources_js, ['scripts']);
 });
