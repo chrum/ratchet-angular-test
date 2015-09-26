0ae9edfba7698d928ef3a3950719a82d4bafed68
chrystian <chrystianr@gmail.com>
2015-09-26 19:13:53 +0200

    Improving dev start script

--- start.sh
+++ start.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 CWD=$(pwd)
+PIDS=()
 
 # Lets ease remote debugging with phpStorm (just start listening for debug connection in IDE)
 export XDEBUG_CONFIG="remote_enable=1 remote_mode=req remote_port=9000 remote_host=127.0.0.1 remote_connect_back=0"
 
 # start webpack dev server
-(cd app && gulp wds) &
+cd app
+gulp wds &
+PIDS+=($!)
+
 # start websockets server and wait for iot
-(php $CWD/server/server.php) &
-WSid=$!
+php $CWD/server/server.php debug &
 #wait for websockets server to finish
+WSid=$!
 wait $WSid
 
 # kill all child processes upon killing this script
-#function killAllSubs {
-#    echo 'Stopping all jobs'
-#    for job in `jobs -p`
-#    do
-#    echo $job
-#        kill $job || let "FAIL+=1"
-#    done
-#}
-#
-#trap killAllSubs EXIT
+function killAllSubs {
+    echo 'Stopping all jobs'
+    for pid in "${PIDS[@]}"
+    do
+        kill $pid
+    done
+}
+
+trap killAllSubs EXIT
